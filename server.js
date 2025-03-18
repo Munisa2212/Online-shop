01a9a8a17c482a8df12a87790312e8f70600f453
BehruzzCoder <BehruzzCoder@gmail.com>
2025-03-19 00:21:44 +0500

    1

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const { connectDB } = require('./config/db')
 const UserRoute = require('./routes/user.route')
 const ProductRoute = require('./routes/product.route')
 const RegionRoute = require('./routes/region.route')
+const CategoryRoute = require("./routes/category.route")
 connectDB()
 
 app.use(express.json())
 app.use('/user', UserRoute)
 app.use('/product', ProductRoute)
 app.use('/region', RegionRoute)
+app.use('/category', CategoryRoute)
 
 app.listen(3000, () => console.log('server is running on port 3000'))
