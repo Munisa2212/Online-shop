0c0ceb3b9c57a9fe08bf7a6c3e09376a10e987d8
BehruzzCoder <BehruzzCoder@gmail.com>
2025-03-18 16:38:11 +0500

    config tayyor

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const {Sequelize} = require('sequelize');
+
+const db = new Sequelize("test","root","1212",{
+    host: "localhost",
+    dialect: "mysql",
+})
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        console.log("Connected to database");
+        await db.sync({force: true});
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+
+module.exports = {db, connectDB};
