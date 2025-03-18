683946b69b893e36e895c119774aa6932621a37c
BehruzzCoder <BehruzzCoder@gmail.com>
2025-03-18 17:22:28 +0500

    module larni togirladim

---
+++ models/index.module.js
@@ -0,0 +1,8 @@
+const product = require("./product.module")
+const category = require("./product.category")
+const region = require("./product.region")
+const user = require("./user.module")
+const order = require("./order.module")
+const comment = require("./comment.module")
+const sequelize  = require("sequelize")
+
