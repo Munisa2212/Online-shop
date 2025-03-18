693081689b75f3939fcc2301d2e3e926fbfc3c52
BehruzzCoder <BehruzzCoder@gmail.com>
2025-03-18 23:58:39 +0500

    serverga regionRoute ni qoshdim

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const app = express();
-const {connectDB} = require('./config/db');
-const UserRoute = require("./routes/user.route")
-const ProductRoute = require("./routes/product.route")
+const express = require('express')
+const app = express()
+const { connectDB } = require('./config/db')
+const UserRoute = require('./routes/user.route')
+const ProductRoute = require('./routes/product.route')
+const RegionRoute = require('./routes/region.route')
 connectDB()
 
-app.use(express.json());
-app.use("/user",UserRoute)
-app.use("/product",ProductRoute)
+app.use(express.json())
+app.use('/user', UserRoute)
+app.use('/product', ProductRoute)
+app.use('/region', RegionRoute)
 
-app.listen(3000,()=>console.log("server is running on port 3000"))
+app.listen(3000, () => console.log('server is running on port 3000'))
