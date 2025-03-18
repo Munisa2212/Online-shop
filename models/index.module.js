e4367391774fe4c65db423c6cdd4c88dcbfeb422
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-18 17:46:44 +0500

    category module done

--- models/index.module.js
+++ models/index.module.js
@@ -1,6 +1,6 @@
 const {product} = require("./product.module")
 const {category} = require("./product.category")
-const {region} = require("./product.region")
+const {Region} = require("./region.module")
 const {user} = require("./user.module")
 const {order} = require("./order.module")
 const {comment} = require("./comment.module")
