eb37ba935d871aa2847c8384369a1e11e04f98f6
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-18 16:54:35 +0500

    order_item module done

---
+++ models/order_item.module.js
@@ -0,0 +1,34 @@
+const joi = require("joi")
+const {db} = require("../config/db")
+const { DataTypes } = require("sequelize")
+
+const Order_item = db.define(
+    "Order_item",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        order_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        count: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }
+)
+
+const Order_item_Validation = joi.object({
+    order_id: joi.number().required(),
+    product_id: joi.number().required(),
+    count: joi.number().required(),
+})
+
+module.exports = {Order_item, Order_item_Validation}
