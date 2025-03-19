dafda2f2279ce2d2e03378705b9bc05e37e902ee
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-19 15:29:50 +0500

    added winston

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ const RegionRoute = require('./routes/region.route')
 const CategoryRoute = require("./routes/category.route")
 const OrderRoute = require("./routes/order.route")
 const CommentRoute = require("./routes/comment.route")
+const winston = require("winston")
 
 connectDB()
+
+const logger = winston.createLogger({
+    level: "silly",
+    format: winston.format.json(),
+    transports: [new winston.transports.File({filename: "logs.log"})]
+})
+
+let categoryLogger = logger.child({module: "product"})
+let commentLogger = logger.child({module: "auth"})
+let orderLogger = logger.child({module: "order"})
+let productLogger = logger.child({module: "product"})
+let regionLogger = logger.child({module: "region"})
+let userLogger = logger.child({module: "user"})
 
 app.use(express.json())
 app.use('/user', UserRoute)
