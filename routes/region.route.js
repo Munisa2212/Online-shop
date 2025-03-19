7077bccde6142189a087ee0d03ddb5c5674562b4
Nurali11 <boltaboyevnurali55@gmail.com>
2025-03-19 16:01:49 +0500

    region.route.js with logger

--- routes/region.route.js
+++ routes/region.route.js
@@ -3,7 +3,8 @@ const express = require('express')
 const route = express.Router()
 const {User} = require("../models/index.module")
 const {Op} = require("sequelize")
- 
+const {regiregionLoggeron, regionLogger} = require("../logger")
+
 route.get('/', async (req, res) => {
   try {
     let {name} = req.query
@@ -20,6 +21,7 @@ route.get('/', async (req, res) => {
     res.status(200).send(regions)
   } catch (error) {
     res.status(500).send(error.message)
+    regionLogger.log("error", "/get error")
   }
 })
 
@@ -38,8 +40,10 @@ route.post('/', async (req, res) => {
       return res.status(400).send({ message: 'Region creation failed' })
     }
     res.status(201).send(region)
+    regionLogger.log("info", "/post")
   } catch (error) {
     res.status(400).send(error.message)
+    regionLogger.log("error", "/post error")
   }
 })
 
@@ -50,8 +54,10 @@ route.get('/:id', async (req, res) => {
       return res.status(404).send({ message: 'Region not found' })
     }
     res.status(200).send(region)
+    regionLogger.log("info", `/get/:id region with ${region.id}`)
   } catch (error) {
     res.status(500).send(error.message)
+    regionLogger.log("error", `/get/:id region error`)
   }
 })
 
@@ -63,8 +69,10 @@ route.put('/:id', async (req, res) => {
     }
     await region.update(req.body)
     res.status(200).send(region)
+    regionLogger.log("info", `/put/:id region with ${region.id}`)
   } catch (error) {
     res.status(500).send(error.message)
+    regionLogger.log("error", `/put/:id region error`)
   }
 })
 
@@ -76,8 +84,10 @@ route.delete('/:id', async (req, res) => {
     }
     await region.destroy()
     res.status(200).send({ message: 'Region deleted successfully' })
+    regionLogger.log("info", `/delete/:id region with ${region.id}`)
   } catch (error) {
     res.status(500).send(error.message)
+    regionLogger.log("error", `/delete/:id region error`)
   }
 })
 
