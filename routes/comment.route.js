8f52efc4214c766046fef95a9870b56d0634671a
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-19 14:00:10 +0500

    comment crud done

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,10 +1,13 @@
-const { Comment } = require("../models/comment.module")
-
+const { CommentValidation } = require("../models/comment.module");
+const { Comment } = require("../models/index.module")
 const app = require("express").Router()
 
 app.post("/", async(req, res)=>{
     const {user_id, product_id, comment} = req.body
     try {
+        let { error } = CommentValidation.validate(req.body);
+        if (error) return res.status(400).send({ error: error.details[0].message });
+
         const data = await Comment.create({user_id, product_id, comment})
         res.send(data)
     } catch (error) {
@@ -12,6 +15,37 @@ app.post("/", async(req, res)=>{
     }
 })
 
+app.get("/", async(req, res)=>{
+    try {
+        const data = await Comment.findAll()
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
 
+app.put("/:id", async(req, res)=>{
+    const {id} = req.params
+    try {
+        let { error } = CommentValidation.validate(req.body);
+        if (error) return res.status(400).send({ error: error.details[0].message });
 
+        const data = await Comment.findByPk(id)
+        await data.update(req.body)
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.delete("/:id", async(req, res)=>{
+    const {id} = req.params
+    try {
+        const data = await Comment.findByPk(id)
+        await data.destroy()
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
 module.exports = app
