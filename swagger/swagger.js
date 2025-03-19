6b1c1c9f23558ff51022081425bdce5a470610c8
BehruzzCoder <BehruzzCoder@gmail.com>
2025-03-19 16:13:38 +0500

    swagger.js qoshildi

---
+++ swagger/swagger.js
@@ -0,0 +1,28 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Online Shop",
+            version: "1.0.0",
+            description: "Online shop where uou can buy everything except humans",
+        },
+        servers: [{ url: "http://localhost:3000" }],
+        components: {
+            securitySchemes: {
+                BearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [{ BearerAuth: [] }],
+    },
+    apis: ["../routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
